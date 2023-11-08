2ce104dd54067ba5b6015db77c2d1b53abac0d34
augustogmc <123521417+augustogmc@users.noreply.github.com>
2023-11-08 15:42:14 -0300

    first commit

---
+++ intensivão JS/main.js
@@ -0,0 +1,85 @@
+/*
+Desafio - 1
+alert("hello world!")
+ */
+
+/* 
+Desafio - 2
+let a = 10
+let b = 5
+
+alert(a + b)
+ */
+
+/* 
+Desafio - 3
+if(typeof(a) == "number"){
+    alert("É um número")
+} else {
+    alert("Não é um número!")
+}
+ */
+
+/* 
+Desafio - 4
+let a = "opa"
+
+if(typeof(a) == "string"){
+    alert("É uma string")
+} else {
+    alert("Não é uma string!")
+}
+ */
+
+/* 
+Desafio - 5
+let a = true
+
+if(typeof(a) == "boolean"){
+    alert("É um booleano")
+} else {
+    alert("Não é um booleano!")
+}
+ */
+
+
+/* Desafio - 6
+
+let a = 10
+let b = 5
+
+alert(a - b) */
+
+/* Desafio - 7
+
+let a = 10
+let b = 5
+
+alert(a * b) */
+
+/* Desafio - 8
+
+let a = 10
+let b = 5
+
+alert(a / b) */
+
+/* Desafio - 9 
+a = 8
+
+if (a % 2 == 0){
+    alert("É um número par")
+} else {
+    alert("Não é um número par")
+}
+ */
+
+/* Desafio - 10
+a = 8
+
+if (a % 2 != 0){
+    alert("É um número impar")
+} else {
+    alert("Não é um número impar")
+}
+ */
